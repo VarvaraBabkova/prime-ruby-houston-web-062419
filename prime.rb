110972df6ee530c8fed457e485679c2f2221b7ca
Varvara Babkova <varvara.munday@gmail.com>
2019-06-06 22:25:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
 # Add  code here!
 def prime? (number)
 
-  if number == 1 || number == 2 || number <= 0
+  if number == 1 || number == 2 
     return true
+  end
+  if number <= 0
+    number = - number
   end
   for i in 2..(number - 1) do
     if number % i == 0
